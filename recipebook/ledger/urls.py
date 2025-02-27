a27d6d3aa135c4523e5cd326bed7721159f839e6
Sofia-Reidenbach <sofia.reidenbach@student.ateneo.edu>
2025-02-28 01:44:40 +0700

    Updated urls.py to have urls for ListView and DetailView

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,14 +1,11 @@
 from django.urls import path
 
-from .views import RecipeList, Recipe1, Recipe2
-
-#from django.contrib import admin
-#from django.urls import include, path
+from .views import RecipeListView, RecipeDetailView
 
 urlpatterns = [
-    path('recipes/list', RecipeList, name='Recipe List'),
-    path('recipe/1', Recipe1, name='Recipe 1'),
-    path('recipe/2', Recipe2, name='Recipe 2')
+    path('',RecipeListView.as_view(),name='recipe_list'),
+    path('recipes/list', RecipeListView.as_view(), name='recipe_list'),
+    path('recipe/<int:pk>', RecipeDetailView.as_view(), name='recipe_detail'),
 ]
-# This might be needed, depending on your Django version
+
 app_name = "ledger"

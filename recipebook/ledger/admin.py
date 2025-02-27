16d030864507dd6f9412e7e98087267327041f9e
Sofia-Reidenbach <sofia.reidenbach@student.ateneo.edu>
2025-02-28 01:35:55 +0700

    Updated admin.py with Recipe and RecipeIngredient

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Recipe, RecipeIngredient
 # Register your models here.
+
+class RecipeIngredientInLine(admin.TabularInline):
+    model = RecipeIngredient
+
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = [RecipeIngredientInLine,]
+    model = Recipe
+
+admin.site.register(Recipe, RecipeAdmin)

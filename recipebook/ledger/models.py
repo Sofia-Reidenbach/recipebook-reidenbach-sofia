f5c79fd23ab157ca9b0165193cca548efa372908
Sofia-Reidenbach <sofia.reidenbach@student.ateneo.edu>
2025-03-10 20:13:24 +0700

    Updated models.py with author, created  on, and updated on

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -15,6 +15,15 @@ class Ingredient(models.Model):
 class Recipe(models.Model):
     name = models.CharField(max_length = 100)
 
+    author = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    
     def __str__(self):
         return self.name
     

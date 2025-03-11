afda7f34a060af74e0b05fe2a7372f1c0ddeca3b
Sofia-Reidenbach <sofia.reidenbach@student.ateneo.edu>
2025-03-11 21:48:41 +0700

    Updated models.py with fixed author field in Recipe model and fixed PEP8 formatting

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,44 +1,41 @@
 from django.db import models
 from django.urls import reverse
 
-# Create your models here    
+# Create your models here
+
 
 class Ingredient(models.Model):
-    name = models.CharField(max_length = 100)
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
-    
+
     def get_absolute_url(self):
         return reverse('ledger:ingredient_detail', args=[str(self.pk)])
 
+
 class Recipe(models.Model):
-    name = models.CharField(max_length = 100)
-
-    author = models.ForeignKey(
-        User,
-        on_delete=models.SET_NULL,
-        null=True
-    )
-
+    name = models.CharField(max_length=100)
+    author = models.CharField(max_length=100, default="Anonymous")
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
-    
+
     def __str__(self):
         return self.name
-    
+
     def get_absolute_url(self):
         return reverse('ledger:recipe_detail', args=[str(self.pk)])
 
+
 class RecipeIngredient(models.Model):
-    quantity = models.CharField(max_length = 100)
+    quantity = models.CharField(max_length=100)
     ingredient = models.ForeignKey(
         'Ingredient',
-        on_delete = models.CASCADE,
+        on_delete=models.CASCADE,
         related_name='recipe'
     )
     recipe = models.ForeignKey(
         'Recipe',
-        on_delete = models.CASCADE,
+        on_delete=models.CASCADE,
         related_name='ingredients'
     )

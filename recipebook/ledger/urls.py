d52ede54ce935ab212156559fb8b0d8c913e9808
Sofia-Reidenbach <sofia.reidenbach@student.ateneo.edu>
2025-03-25 01:25:43 +0700

    Update urls.py with paths to recipe image upload and recipe create

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import RecipeListView, RecipeDetailView
+from .views import RecipeListView, RecipeDetailView, RecipeCreateView, RecipeImageUploadView
 
 urlpatterns = [
     path('',RecipeListView.as_view(),name='recipe_list'),
     path('recipes/list', RecipeListView.as_view(), name='recipe_list'),
     path('recipe/<int:pk>', RecipeDetailView.as_view(), name='recipe_detail'),
+    path('recipe/<int:pk>/add_image/',
+         RecipeImageUploadView.as_view(), name='recipe_image_upload'),
+    path('recipe/add', RecipeCreateView.as_view(), name="recipe_add")
 ]
 
 app_name = "ledger"

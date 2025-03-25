10bd210d9c83adab6de1951ec7a1c1329b7d6dac
Sofia-Reidenbach <sofia.reidenbach@student.ateneo.edu>
2025-03-25 09:50:45 +0700

    Create forms.py to process entered images and new recipes

---
+++ recipebook/ledger/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import Recipe, RecipeImage
+
+
+class RecipeForm(forms.ModelForm):
+    class Meta:
+        model = Recipe
+        fields = '__all__'
+        widgets = {
+            'created_on': forms.TextInput(attrs={'type': 'datetime-local'}),
+            'updated_on': forms.TextInput(attrs={'type': 'datetime-local'}),
+        }
+
+
+class RecipeImageForm(forms.ModelForm):
+    class Meta:
+        model = RecipeImage
+        fields = ["image", "description",]
